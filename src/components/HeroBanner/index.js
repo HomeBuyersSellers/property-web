6b8b41cd56838047764f01cdef844edfc976691c
Lakshya Choudhary <lakshya.choudhary-c@jumeirah.com>
2023-09-28 11:38:19 +0530

    Hero map upated

--- src/components/HeroBanner/index.js
+++ src/components/HeroBanner/index.js
@@ -15,9 +15,19 @@ useEffect(()=>{
   console.log('Lg:',longitude,'Lt:',latitude)
   return (
     <section className="mb-10 relative h-96 w-full bg-slate-400">
-      <div className="bg-cover bg-center bg-no-repeat bg-hero-banner h-full">
-      <HeroMap latitude={latitude} longitude={longitude}/>
+    <div className="bg-cover bg-center bg-no-repeat h-full relative">
+    <HeroMap latitude={latitude} longitude={longitude} />
+    <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-center w-full max-w-[800px] px-6">
+      <h2 className="mb-12 text-5xl font-bold leading-tight tracking-tight md:text-6xl xl:text-7xl">
+        Are you ready <br /><span>for an adventure</span>
+      </h2>
+      <p className="text-lg">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Minima officia consequatur adipisci tenetur repudiandae rerum
+        quos.
+      </p>
       </div>
+    </div>
     </section>
   );
 };
